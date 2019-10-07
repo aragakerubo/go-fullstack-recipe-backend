db4c78aa62b968703c4dc0300d5d4d9cab94116f
ogambakerubo <ogambahilda@gmail.com>
2019-10-08 01:32:50 +0300

    Create files to run the backend app   - .env   - server.js   - app.js   - model/recipe.js   - .gitignore   - README.md

---
+++ README.md
@@ -0,0 +1,49 @@
+# Project code for Open Classrooms course
+
+Clone this repo and add Gulp tasks as set out in the course activity.
+
+## Getting started
+
+- Clone the repo
+
+```bash
+git clone https://github.com/aragakerubo/5493201-js-for-the-web-gulp.git
+```
+
+- Install the package
+
+```bash
+npm install
+```
+
+- Create a `.env` in the root directory file and fill in the with the appropriate values and save the changes
+
+```bash
+MONGODB_URI=mongodb://<USERNAME>:<PASSWORD>@<CLUSTER>-shard-00-00-dxpxl.mongodb.net:27017,<CLUSTER>-shard-00-01-dxpxl.mongodb.net:27017,<CLUSTER>-shard-00-02-dxpxl.mongodb.net:27017/test?ssl=true&replicaSet=<CLUSTER>-shard-0&authSource=admin&retryWrites=true&w=majority
+```
+
+- Now run the following command to start the app from the script object
+
+```bash
+npm start
+```
+
+- And voila! Make changes and watch them update in real time.
+
+### NB
+
+Make sure you already have gulp installed globally. For macs/ linux add the `sudo` command incase of write access errors.
+
+```bash
+npm install -g nodemon
+```
+
+## Endpoints
+
+| URI                          | URI Parameters                                                   | Message Body Input | Description                                               | Requires Authentication? |
+| :--------------------------- | :--------------------------------------------------------------- | :----------------- | :-------------------------------------------------------- | :----------------------- |
+| GET <br> /api/recipes        | None                                                             | None               | Returns all recipes in database                           | No                       |
+| GET <br> /api/recipes/:id    | `id` (required): the recipe id                                   | None               | Returns the recipe with the provided ID from the database | Yes                      |
+| POST <br> /api/recipes       | None                                                             | Yes                | Adds a new recipe to the database                         | No                       |
+| PUT <br> /api/recipes/:id    | `id` (required): The unique ID of the recipe you want to update. | Yes                | Modifies the recipe with the provided ID                  | Yes                      |
+| DELETE <br> /api/recipes/:id | `id` (required): The ID of the recipes you want to delete        | None               | Deletes the recipe with the provided ID                   | No                       |
---
+++ app.js
@@ -0,0 +1,137 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  dotenv = require("dotenv"),
+  Recipe = require("./models/recipe");
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+  })
+  .catch(error => {
+    console.log("Unable to connect to MongoDB Atlas!");
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post("/api/recipes", (req, res, next) => {
+  const recipe = new Recipe({
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time
+  });
+
+  recipe
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Post saved successfully!"
+      });
+    })
+    .catch(error => {
+      console.error(error);
+
+      res.status(400).json({
+        error: error
+      });
+    });
+});
+
+app.get("/api/recipes/:id", (req, res, next) => {
+  Recipe.findOne({
+    _id: req.params.id
+  })
+    .then(recipe => {
+      res.status(200).json(recipe);
+    })
+    .catch(error => {
+      console.error(error);
+
+      res.status(404).json({
+        error: error
+      });
+    });
+});
+
+app.put("/api/recipes/:id", (req, res, next) => {
+  const recipe = new Recipe({
+    _id: req.params.id,
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time
+  });
+
+  Recipe.updateOne({ _id: req.params.id }, recipe)
+    .then(() => {
+      res.status(201).json({
+        message: "Recipe updated successfully!"
+      });
+    })
+    .catch(error => {
+      console.error(error);
+
+      res.status(400).json({
+        error: error
+      });
+    });
+});
+
+app.delete("/api/recipes/:id", (req, res, next) => {
+  Recipe.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Deleted!"
+      });
+    })
+    .catch(error => {
+      console.error(error);
+
+      res.status(400).json({
+        error: error
+      });
+    });
+});
+
+app.use("/api/recipes", (req, res, next) => {
+  Recipe.find()
+    .then(recipes => {
+      res.status(200).json(recipes);
+    })
+    .catch(error => {
+      console.error(error);
+
+      res.status(400).json({
+        error: error
+      });
+    });
+});
+
+module.exports = app;
